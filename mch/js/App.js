64decd274ee0526d99e65ac6e84697d2470fc3e1
mozent <mozent1@yahoo.com.tw>
2021-08-26 22:16:15 +0800

    s

--- mch/js/App.js
+++ mch/js/App.js
@@ -1,11 +1,14 @@
 
 $( document ).ready(function() {
     $('ul.navigation').find('li').hover(function () {
-        console.log($(this).position().left +"px");
-        $(this).find(".submenu").css('left',($(this).position().left+6) +"px");        
-    }
-    /* , function () {
+       // console.log($(this).position().left +"px");
+        $(this).find(".submenu").css('left',($(this).position().left+6) +"px")
+                .css('top',($(this).position().top + $(this).outerHeight()) +"px"); 
+    });
+       
+    /*}
+     , function () {
         $(this).removeClass('hover');
-    } */
-    );
+    }
+    ); */
 });
